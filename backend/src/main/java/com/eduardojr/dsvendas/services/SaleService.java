38242d6f7e8c70dc0761747c54ea88f636bdb515
eduardomsj <eduardomsj@gmail.com>
2021-09-09 20:28:51 -0300

    Pagination

---
+++ backend/src/main/java/com/eduardojr/dsvendas/services/SaleService.java
@@ -0,0 +1,29 @@
+package com.eduardojr.dsvendas.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.eduardojr.dsvendas.dto.SaleDTO;
+import com.eduardojr.dsvendas.entities.Sale;
+import com.eduardojr.dsvendas.repositories.SaleRepository;
+import com.eduardojr.dsvendas.repositories.SellerRepository;
+
+@Service
+public class SaleService {
+
+	@Autowired
+	private SaleRepository repository;
+	
+	@Autowired
+	private SellerRepository sellerRepository;
+
+	@Transactional(readOnly = true)
+	public Page<SaleDTO> findAll(Pageable pageable) {
+		sellerRepository.findAll();
+		Page<Sale> result = repository.findAll(pageable);
+		return result.map(x -> new SaleDTO(x));
+	}	
+}
